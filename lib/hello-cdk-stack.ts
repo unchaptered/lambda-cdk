28b6669fa2de66b7bc826ffaa78b9a20d9dbdbab
unchaptered <workstation19961002@gmail.com>
2022-11-05 19:57:48 +0900

    Update HelloCdkQueue in HelloCdkStack

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,9 +1,14 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const queue = new Queue(this, 'HelloCdkQueue', {
+      visibilityTimeout: Duration.seconds(300)
+    });
 
   }
 }
