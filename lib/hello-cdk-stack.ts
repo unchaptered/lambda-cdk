8383a35bd112b9a2a27ede3563d384234ecf734b
unchaptered <workstation19961002@gmail.com>
2022-11-05 19:57:16 +0900

    Rollback into initial setup

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,21 +1,9 @@
-import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);    
-
-    const queue = new Queue(this, 'HelloCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const newBucket = new Bucket(this, 'MyFirstBucket', {
-      versioned: true,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true
-    });
+    super(scope, id, props);
 
   }
 }
