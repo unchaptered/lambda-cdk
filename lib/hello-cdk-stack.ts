c877d4316e61e2c72f63894fe4a53ddf57fb6669
unchaptered <workstation19961002@gmail.com>
2022-11-05 21:34:52 +0900

    Update MyFirstbucket in HelloCdkStack

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,4 +1,5 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 
@@ -8,6 +9,16 @@ export class HelloCdkStack extends Stack {
 
     const queue = new Queue(this, 'HelloCdkQueue', {
       visibilityTimeout: Duration.seconds(300)
+    });
+
+    const s3Bucket = new Bucket(this, 'MyFirstbucket', {
+      versioned: true,
+      /*
+       * When stack is destoryed, S3 bucket isn't destoryed(default).
+       * So, this options must be true to build Pracitce App with AWS-CDK
+       */
+      removalPolicy:  RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
     });
 
   }
